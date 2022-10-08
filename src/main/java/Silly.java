e8e53659b8c71b44550c434c2efc6ef6498eef80
jeffhu <jeff1hu@gmail.com>
2022-10-07 22:15:36 -0400

    complete

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -79,10 +79,17 @@ public class Silly implements Comparable<Silly>{
      *       strings (e.g. this.name = [first string] + [second string]).
      *       Make sure you document this method!
      */
+    /**
+    * Creates a silly object
+    * This constructor takes in two strings as arguments
+    *
+    * @param s1 the first string
+     * @param s2 the second string
+     */
 
-
-
-
+    public Silly(String s1, String s2){
+        this.name = s1 + s2;
+    }
 
     public static void main(String[] args) {
         /**
@@ -116,7 +123,7 @@ public class Silly implements Comparable<Silly>{
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {};
+        int[] expected_values = {0, 1,2, 3};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
@@ -133,7 +140,7 @@ public class Silly implements Comparable<Silly>{
      */
     @Override
     public String toString(){
-        // TODO (Task 3): Implement the body of this method!
+        return this.name;
     }
 
     /**
@@ -158,6 +165,7 @@ public class Silly implements Comparable<Silly>{
         }
 
         Silly other = (Silly) o; // To access .name of o, we need to cast it.
+        return this.name.equals(other.name);
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
@@ -185,6 +193,14 @@ public class Silly implements Comparable<Silly>{
      */
     @Override
     public int compareTo(Silly other) {
+        if (this.name.length() > other.name.length()) {
+            return 1;
+        } else if (this.name.length() < other.name.length()){
+            return -1;
+        } else {
+            return 0;
+        }
+
         /**
          * TODO (Task 5): Implement the body of this method.
          *                A positive number should be returned if this.name
